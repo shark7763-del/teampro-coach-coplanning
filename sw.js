fe106bde8ac604a218890082aabf55b8780201d2
shark7763 <shark7763@gmail.com>
2026-08-16 08:38:12 +0800

    Add editable PPT lesson summaries

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const APP_VERSION = '1.18.3-internal';
-const CACHE_NAME = 'teampro-coplanning-v38';
+const APP_VERSION = '1.18.4-internal';
+const CACHE_NAME = 'teampro-coplanning-v39';
 const STATIC_ASSETS = [
   './manifest.webmanifest',
   './共同備課logo.png',
